47f0b7563aa7c32317c41e6f9588c0b145943501
gunay <gqasimova93@gmail.com>
2024-04-25 21:22:15 +0400

    added handler for error

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,11 @@ def teardown(exc):
     storage.close()
 
 
+@app.errorhandler(404)
+def error():
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', 5000)
