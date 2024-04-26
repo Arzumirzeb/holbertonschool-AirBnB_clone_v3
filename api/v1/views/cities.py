3be01824bb28664de674154cb732fded13b84808
arzumirzeb <arzumirzeb03@gmail.com>
2024-04-27 00:01:42 +0400

    pycostyle of cities.py fixed

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,6 +7,7 @@ from models import storage
 from models.city import City
 from models.state import State
 
+
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def get_cities_with_id(state_id):
@@ -36,7 +37,8 @@ def delete_city(city_id):
     return {}, 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     state = storage.get(State, state_id)
     if state is None:
